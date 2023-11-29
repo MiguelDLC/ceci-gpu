b3f356c762049f6f97f4280a20d650eeff928942
Miguel De Le Court <migueldelele@gmail.com>
2023-11-29 21:15:30 +0100

    Simplify code structure

--- main.cu
+++ main.cu
@@ -3,154 +3,6 @@
 #include <math.h>
 #include <chrono>
 
-enum {_X=0,_Y,_BATH,_GAMMA,_COR,_TAUX,_TAUY,_C,_NF};
-
-template <typename T>
-T* malloc_manged_flags(size_t count){
-  T* ptr;
-  ERRCHK(cudaMallocManaged(&ptr, count*sizeof(T)));
-  ERRCHK(cudaMemAdvise(ptr, count, cudaMemAdviseSetPreferredLocation, cudaMemLocationTypeDevice));
-  ERRCHK(cudaMemAdvise(ptr, count, cudaMemAdviseSetAccessedBy, cudaMemLocationTypeDevice));
-  ERRCHK(cudaMemset(ptr, 0, count*sizeof(T)));
-  return ptr;
-}
-
-
-int readmesh(const char* filename, Array3D<scalar,_NF> &data, Array3D<int,2> &neighbours){
-  FILE *fp = fopen("square.txt","r");
-  int n_elem;
-  fscanf(fp,"%d",&n_elem);
-  data.data = malloc_manged_flags<scalar>(_NF*3*n_elem);
-  data.n_elem = n_elem;
-  neighbours.data = malloc_manged_flags<int>(2*3*n_elem);
-  neighbours.n_elem = n_elem;
-
-  for(int elem = 0; elem < n_elem; elem++){
-    for(int node = 0; node < 3; node++){
-      double x=NAN, y=NAN, bath=NAN, gamma=NAN, cor=NAN, taux=NAN, tauy=NAN, c=NAN;
-      int n=2147483647, cl=2147483647;
-      fscanf(fp,"%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%d,%d",&x,&y,&bath,&gamma,&cor,&taux,&tauy,&c,&n,&cl);
-      data(_X,node,elem) = x;
-      data(_Y,node,elem) = y;
-      data(_BATH,node,elem) = bath;
-      data(_GAMMA,node,elem) = gamma;
-      data(_COR,node,elem) = cor;
-      data(_TAUX,node,elem) = taux;
-      data(_TAUY,node,elem) = tauy;
-      data(_C,node,elem) = c;
-      neighbours(0,node,elem) = n;
-      neighbours(1,node,elem) = cl;
-    }
-  }
-  fclose(fp);
-  return n_elem;
-}
-
-void write_result(const char* filename, Array3D<scalar,3> &solution){
-  FILE *fp = fopen(filename,"w");
-  int n_elem = solution.n_elem;
-  for(int elem = 0; elem < n_elem; elem++){
-    for(int node = 0; node < 3; node++){
-      fprintf(fp,"%e,%e,%e\n", solution(0,node,elem), solution(1,node,elem), solution(2,node,elem));
-    }
-  }
-  fclose(fp);
-}
-
-
-__device__ inline static scalar det2x2(const scalar mat[2][2]) {
-  return mat[0][0]*mat[1][1]-mat[0][1]*mat[1][0];
-}
-
-__device__ inline static scalar inv2x2(const scalar mat[2][2], scalar inv[2][2])
-{
-  scalar det = det2x2(mat);
-  scalar ud = 1.f / det;
-  inv[0][0] =  mat[1][1] * ud;
-  inv[1][0] = -mat[1][0] * ud;
-  inv[0][1] = -mat[0][1] * ud;
-  inv[1][1] =  mat[0][0] * ud;
-  return det;
-}
-
-template<typename Array>
-__device__ void inline static fe_2d_triangle(Array &xy, int elem, scalar dphi[3][2], scalar *jac) {
-  scalar dxdxi[2][2]={{0}};
-  scalar dxidx[2][2];
-  for (int node = 0; node < 3; node++) {
-    dxdxi[0][0] += xy(0, node, elem)*tri_dphi_dxi[node][0];
-    dxdxi[0][1] += xy(0, node, elem)*tri_dphi_dxi[node][1];
-    dxdxi[1][0] += xy(1, node, elem)*tri_dphi_dxi[node][0];
-    dxdxi[1][1] += xy(1, node, elem)*tri_dphi_dxi[node][1];
-  }
-  *jac = inv2x2(dxdxi,dxidx);
-  for (int node = 0; node < 3; node++) {
-    for (int j = 0; j < 2; ++j) {
-      dphi[node][j] = tri_dphi_dxi[node][0]*dxidx[0][j] + tri_dphi_dxi[node][1]*dxidx[1][j];
-    }
-  } 
-}
-
-template<typename Array>
-__device__ inline static scalar fe_2d_interp_field(Array &solution, int field, int ie, const scalar phi[3]){
-  scalar v = 0;
-  for (int node = 0; node < 3; ++node) {
-    v += solution(field, node, ie)*phi[node];
-  }
-  return v;
-}
-
-template<typename Array>
-__device__ inline static void fe_2d_assemble_term(Array &rhse,int nphi, int nf, scalar *phi, scalar dphi[3][2], scalar w, scalar *s) {
-  for (int node = 0; node < nphi; node++) { 
-    for (int field = 0; field < nf; ++field) {
-      scalar r = phi[node]*s[field];
-      if(dphi) 
-        r += dphi[node][0]*s[nf+field*2+0]+dphi[node][1]*s[nf+field*2+1];
-      rhse(field, node, 0) += w*r;
-    }
-  }
-}
-
-__device__ inline static void tri_phi(const scalar pts[2], scalar *phi) {
-  scalar xi = pts[0], eta = pts[1];
-  phi[0] = 1-xi-eta;
-  phi[1] = xi;
-  phi[2] = eta;
-}
-
-__device__ inline static void tri_phi_e(scalar pts[2], scalar *phi) {
-  scalar xi = pts[0], eta = pts[1];
-  phi[0] = 1-xi-eta;
-  phi[1] = xi;
-  phi[2] = eta;
-}
-
-template<typename Array>
-__device__ inline static void fe_2d_grad_field(Array &solution, int field, int ie, const scalar dphi[3][2], scalar v[2]){
-  for (int k = 0; k < 2; ++k){
-    v[k] = 0;
-  }
-  for (int node = 0; node < 3; ++node) {
-    for (int k = 0; k < 2; ++k){
-      v[k] += solution(field, node, ie)*dphi[node][k];
-    }
-  }
-}
-
-__device__ inline static void fe_2d_multiply_inv_mass_matrix(scalar jac, LocalArray3D<3> &rhse) {
-  //multiply by inv mass matrix
-  for (int ifield = 0; ifield < 3; ++ifield) {
-    scalar rhsl[3];
-    for (int inode = 0; inode <3; ++inode) {
-      rhsl[inode] = rhse(ifield, inode);
-    }
-    for (int inode = 0; inode <3; ++inode) {
-      rhse(ifield, inode) = (-6*(rhsl[0]+rhsl[1]+rhsl[2])+24*rhsl[inode])/jac;
-    }
-  }
-}
-
 __global__ void dudt(
   Array3D<scalar,3> solution,
   Array3D<int,2> neighbours,
@@ -184,12 +36,6 @@ __global__ void dudt(
       nx = -(y0-y1)/length;
       ny = (x0-x1)/length;
 
-      
-      LocalArray3D<3> solri;
-      if (tri_r >= 0) {
-        solri = load_elem(solution, tri_r);
-      }
-      
       for (int iq = 0; iq < gauss_edge_n; ++iq) {
         scalar phil[3];
         scalar xitl[2];
@@ -256,20 +102,19 @@ __global__ void dudt(
       scalar dbath[2];
       fe_2d_grad_field(data, _BATH, ie, dphi, dbath);
 
-      scalar s[9] = {0.f};
+      scalar s[9] = {0};
       fvolume(sol, dsol, bath, dbath, cor, tau, s, g, rho_0, gamma);
 
       scalar w = gauss_tri_w[iq]*jac;
       fe_2d_assemble_term(rhsi,3,3,phi,dphi,w,s);
-
     }
 
     // inverse mass matrix
     fe_2d_multiply_inv_mass_matrix(jac,rhsi);
 
     // write back
-    for (int field = 0; field < 3; ++field) {
-      for (int node = 0; node < 3; ++node) {
+    for (int field = 0; field <3; ++field) {
+      for (int node = 0; node <3; ++node) {
         rhs(field, node, ie) = rhsi(field, node);
       }
     }
@@ -311,10 +156,6 @@ int main(){
   for(int iter = 0; iter < niter; iter++){
     int nsub = 1000;
     for(int i = 0; i < nsub; i++){
-      // classical Explicit Euler
-      // dudt<<<ngrid_dudt,BLOCK_SIZE>>>(solution, neighbours, data, 9.81, 1000, rhs, n_elem);
-      // axpy<<<ngrid_axpy,BLOCK_SIZE>>>(solution.data, rhs.data, solution.data, 1, dt, n_elem*9);
-
       // classical RK2
       dudt<<<ngrid_dudt,BLOCK_SIZE>>>(solution, neighbours, data, 9.81, 1000, rhs, n_elem);
       axpy<<<ngrid_axpy,BLOCK_SIZE>>>(solution.data, rhs.data, solution_mid.data, 1, dt/2, n_elem*9);
@@ -337,7 +178,7 @@ int main(){
   }
   double dt_mean = dt_sum1/niter;
   double dt_std = sqrt(dt_sum2/niter - dt_mean*dt_mean);
-  printf("Min time per iter : %.1f us\n", dt_min*1e6);
+  printf("Min  time per iter: %.1f us\n", dt_min*1e6);
   printf("Mean time per iter: %.1f us\n", dt_mean*1e6);
   printf("Std : %.3f us\n", dt_std*1e6);
   return 0;
--- main.h
+++ main.h
@@ -1,8 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <cuda_runtime.h>
-typedef float scalar;
+
+typedef double scalar;
 #define BLOCK_SIZE 128
 
+enum {_X=0,_Y,_BATH,_GAMMA,_COR,_TAUX,_TAUY,_C,_NF};
 template<typename T, int n_fields>
 class Array3D {
   public:
@@ -15,24 +19,7 @@ class Array3D {
   }
 };
 
-template<int n_fields>
-class SharedArray3D {
-  public:
-  scalar data[BLOCK_SIZE*n_fields*3];
-  __device__ constexpr static int num_fields(){return n_fields;};
-  __device__ inline scalar &operator()(int field, int node, int elem) {
-    return data[BLOCK_SIZE*3*field + BLOCK_SIZE*node + elem];
-  }
-};
-
-#define ERRCHK(err) (errchk(err, __FILE__, __LINE__ ))
-static inline void errchk(cudaError_t err, const char *file, int line) {
-  if (err != cudaSuccess) {
-    fprintf(stderr, "\n\n%s in %s:%d\n", cudaGetErrorString(err), file, line);
-    exit(EXIT_FAILURE);
-  }
-}
-
+// good class, no changes needed
 template<int n_fields>
 class LocalArray3D {
   public:
@@ -49,46 +36,6 @@ class LocalArray3D {
   }
 };
 
-template<int n_fields>
-__device__ LocalArray3D<n_fields> load_elem(Array3D<scalar, n_fields> &array, int elem) {
-  LocalArray3D<n_fields> local;
-  for(int field = 0; field < n_fields; field++) {
-    for(int node = 0; node < 3; node++) {
-      local(field, node) = array(field, node, elem);
-    }
-  }
-  return local;
-}
-
-template<int n_fields>
-__device__ void store_elem(Array3D<scalar, n_fields> &array, int elem, LocalArray3D<n_fields> &local) {
-  for(int field = 0; field < n_fields; field++) {
-    for(int node = 0; node < 3; node++) {
-      array(field, node, elem) = local(field, node);
-    }
-  }
-}
-
-
-
-
-#define gauss_tri_n 6
-__device__ __constant__ static const scalar gauss_tri_xi[gauss_tri_n][2] = {
-  {0.659027622374092, 0.231933368553031},
-  {0.659027622374092, 0.109039009072877},
-  {0.231933368553031, 0.659027622374092},
-  {0.231933368553031, 0.109039009072877},
-  {0.109039009072877, 0.659027622374092},
-  {0.109039009072877, 0.231933368553031}};
-__device__ __constant__ static const scalar gauss_tri_w[gauss_tri_n] = {1./12,1./12,1./12,1./12,1./12,1./12};
-
-#define gauss_edge_n 2
-__device__ const scalar gauss_edge_xi[2] = { -0.5773502691896257, 0.5773502691896257};
-__device__ const scalar gauss_edge_w[2] = {1.0,1.0};
-
-__device__ __constant__ static const int closure_cl[6][4] = {{0,1,4,3}, {1,2,5,4}, {2,0,3,5},{1,0,3,4}, {2,1,4,5}, {0,2,5,3}};
-__device__ __constant__ static const scalar tri_dphi_dxi[3][2] = {{-1,-1},{1,0},{0,1}};
-
 __device__ inline static void fe_2d_closure_xi(scalar xie, int cl, scalar xit[2]) {
   scalar x = (1+xie)/2.0;
   switch (cl) {
@@ -98,7 +45,42 @@ __device__ inline static void fe_2d_closure_xi(scalar xie, int cl, scalar xit[2]
     case 3 : xit[0]=1-x; xit[1]=  0; break;
     case 4 : xit[0]=  x; xit[1]=1-x; break;
     case 5 : xit[0]=  0; xit[1]=  x; break;
-    default: printf("fe_2d_closure_xi: cl %d\n", cl);
+    default: printf("Invalid closure in fe_2d_closure_xi: cl %d\n", cl);
+  }
+}
+
+__device__ inline static scalar det2x2(const scalar mat[2][2]) {
+  return mat[0][0]*mat[1][1]-mat[0][1]*mat[1][0];
+}
+
+__device__ inline static scalar inv2x2(const scalar mat[2][2], scalar inv[2][2]){
+  scalar det = det2x2(mat);
+  if(det == 0){
+    printf("Singular matrix in inv2x2\n");
+    inv[0][0] = 0;
+    inv[1][0] = 0;
+    inv[0][1] = 0;
+    inv[1][1] = 0;
+    return 0;
+  }
+  scalar ud = 1.f / det;
+  inv[0][0] =  mat[1][1] * ud;
+  inv[1][0] = -mat[1][0] * ud;
+  inv[0][1] = -mat[0][1] * ud;
+  inv[1][1] =  mat[0][0] * ud;
+  return det;
+}
+
+__device__ inline static void fe_2d_multiply_inv_mass_matrix(scalar jac, LocalArray3D<3> &rhse) {
+  //multiply by inv mass matrix
+  for (int ifield = 0; ifield <3; ++ifield) {
+    scalar rhsl[3];
+    for (int inode = 0; inode <3; ++inode) {
+      rhsl[inode] = rhse(ifield, inode);
+    }
+    for (int inode = 0; inode <3; ++inode) {
+      rhse(ifield, inode) = (-6.0*(rhsl[0]+rhsl[1]+rhsl[2])+24.0*rhsl[inode])/jac;
+    }
   }
 }
 
@@ -141,6 +123,9 @@ __device__ void inline static iflux(scalar c_l, scalar c_r, scalar soll[3], scal
   flux[2] = flux_v;
 }
 
+/////////// END OF THE EXERCISE, NO CHANGES NEEDED BELOW ///////////
+
+// good function, no changes needed
 __device__ inline static void fvolume(scalar sol[3], scalar dsol[3][2], scalar bath, scalar dbath[2], scalar cor, scalar tau[2], scalar s[9], scalar g, scalar rho_0, scalar gamma) {
   for (int i = 0; i<9; ++i) s[i] = 0;
   scalar *f = s+3;
@@ -149,8 +134,8 @@ __device__ inline static void fvolume(scalar sol[3], scalar dsol[3][2], scalar b
   scalar *Hu = sol+1;
 
   // wave equation
-  s[1] += - g*H*dsol[0][0]; // - datm_press[0]/rho_0;
-  s[2] += - g*H*dsol[0][1]; // - datm_press[1]/rho_0;
+  s[1] += - g*H*dsol[0][0];
+  s[2] += - g*H*dsol[0][1];
   f[0*2+0] += Hu[0];
   f[0*2+1] += Hu[1];
 
@@ -167,3 +152,181 @@ __device__ inline static void fvolume(scalar sol[3], scalar dsol[3][2], scalar b
   f[2*2+0] += u[1]*Hu[0];
   f[2*2+1] += u[1]*Hu[1];
 }
+
+/////////// CUDA error checking, no changes needed ///////////
+#define ERRCHK(err) (errchk(err, __FILE__, __LINE__ ))
+static inline void errchk(cudaError_t err, const char *file, int line) {
+  if (err != cudaSuccess) {
+    fprintf(stderr, "\n\n%s in %s:%d\n", cudaGetErrorString(err), file, line);
+    exit(EXIT_FAILURE);
+  }
+}
+
+/////////// Gauss quadrature, no changes needed ///////////
+#define gauss_tri_n 6
+__device__ __constant__ static const scalar gauss_tri_xi[gauss_tri_n][2] = {
+  {0.659027622374092, 0.231933368553031},
+  {0.659027622374092, 0.109039009072877},
+  {0.231933368553031, 0.659027622374092},
+  {0.231933368553031, 0.109039009072877},
+  {0.109039009072877, 0.659027622374092},
+  {0.109039009072877, 0.231933368553031}};
+__device__ __constant__ static const scalar gauss_tri_w[gauss_tri_n] = {1./12,1./12,1./12,1./12,1./12,1./12};
+
+#define gauss_edge_n 2
+__device__ const scalar gauss_edge_xi[2] = { -0.5773502691896257, 0.5773502691896257};
+__device__ const scalar gauss_edge_w[2] = {1.0,1.0};
+
+__device__ __constant__ static const int closure_cl[6][4] = {{0,1,4,3}, {1,2,5,4}, {2,0,3,5},{1,0,3,4}, {2,1,4,5}, {0,2,5,3}};
+__device__ __constant__ static const scalar tri_dphi_dxi[3][2] = {{-1,-1},{1,0},{0,1}};
+
+
+/////////// Helper functions, no changes needed ///////////
+template <typename T>
+static T* malloc_manged_flags(size_t count){
+  T* ptr;
+  ERRCHK(cudaMallocManaged(&ptr, count*sizeof(T)));
+  ERRCHK(cudaMemAdvise(ptr, count, cudaMemAdviseSetPreferredLocation, cudaMemLocationTypeDevice));
+  ERRCHK(cudaMemAdvise(ptr, count, cudaMemAdviseSetAccessedBy, cudaMemLocationTypeDevice));
+  ERRCHK(cudaMemset(ptr, 0, count*sizeof(T)));
+  return ptr;
+}
+
+static int readmesh(const char* filename, Array3D<scalar,_NF> &data, Array3D<int,2> &neighbours){
+  FILE *fp = fopen("square.txt","r");
+  int n_elem;
+  fscanf(fp,"%d",&n_elem);
+  data.data = malloc_manged_flags<scalar>(_NF*3*n_elem);
+  data.n_elem = n_elem;
+  neighbours.data = malloc_manged_flags<int>(2*3*n_elem);
+  neighbours.n_elem = n_elem;
+
+  for(int elem = 0; elem < n_elem; elem++){
+    for(int node = 0; node <3; node++){
+      double x=NAN, y=NAN, bath=NAN, gamma=NAN, cor=NAN, taux=NAN, tauy=NAN, c=NAN;
+      int n=2147483647, cl=2147483647;
+      fscanf(fp,"%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%d,%d",&x,&y,&bath,&gamma,&cor,&taux,&tauy,&c,&n,&cl);
+      data(_X,node,elem) = x;
+      data(_Y,node,elem) = y;
+      data(_BATH,node,elem) = bath;
+      data(_GAMMA,node,elem) = gamma;
+      data(_COR,node,elem) = cor;
+      data(_TAUX,node,elem) = taux;
+      data(_TAUY,node,elem) = tauy;
+      data(_C,node,elem) = c;
+      neighbours(0,node,elem) = n;
+      neighbours(1,node,elem) = cl;
+    }
+  }
+  fclose(fp);
+  return n_elem;
+}
+
+static void write_result(const char* filename, Array3D<scalar,3> &solution){
+  FILE *fp = fopen(filename,"w");
+  int n_elem = solution.n_elem;
+  for(int elem = 0; elem < n_elem; elem++){
+    for(int node = 0; node <3; node++){
+      fprintf(fp,"%e,%e,%e\n", solution(0,node,elem), solution(1,node,elem), solution(2,node,elem));
+    }
+  }
+  fclose(fp);
+}
+
+template<int n_fields>
+__device__ LocalArray3D<n_fields> load_elem(Array3D<scalar, n_fields> &array, int elem) {
+  LocalArray3D<n_fields> local;
+  for(int field = 0; field < n_fields; field++) {
+    for(int node = 0; node < 3; node++) {
+      local(field, node) = array(field, node, elem);
+    }
+  }
+  return local;
+}
+
+template<int n_fields>
+__device__ void store_elem(Array3D<scalar, n_fields> &array, int elem, LocalArray3D<n_fields> &local) {
+  for(int field = 0; field < n_fields; field++) {
+    for(int node = 0; node < 3; node++) {
+      array(field, node, elem) = local(field, node);
+    }
+  }
+}
+
+
+template<int n_fields>
+class SharedArray3D {
+  public:
+  scalar data[BLOCK_SIZE*n_fields*3];
+  __device__ constexpr static int num_fields(){return n_fields;};
+  __device__ inline scalar &operator()(int field, int node, int elem) {
+    return data[BLOCK_SIZE*3*field + BLOCK_SIZE*node + elem];
+  }
+};
+
+template<typename Array>
+__device__ inline static scalar fe_2d_interp_field(Array &solution, int field, int ie, const scalar phi[3]){
+  scalar v = 0;
+  for (int node = 0; node <3; ++node) {
+    v += solution(field, node, ie)*phi[node];
+  }
+  return v;
+}
+
+template<typename Array>
+__device__ void inline static fe_2d_triangle(Array &xy, int elem, scalar dphi[3][2], scalar *jac) {
+  scalar dxdxi[2][2]={{0}};
+  scalar dxidx[2][2];
+  for (int node = 0; node <3; node++) {
+    dxdxi[0][0] += xy(0, node, elem)*tri_dphi_dxi[node][0];
+    dxdxi[0][1] += xy(0, node, elem)*tri_dphi_dxi[node][1];
+    dxdxi[1][0] += xy(1, node, elem)*tri_dphi_dxi[node][0];
+    dxdxi[1][1] += xy(1, node, elem)*tri_dphi_dxi[node][1];
+  }
+  *jac = inv2x2(dxdxi,dxidx);
+  for (int node = 0; node <3; node++) {
+    for (int j = 0; j < 2; ++j) {
+      dphi[node][j] = tri_dphi_dxi[node][0]*dxidx[0][j] + tri_dphi_dxi[node][1]*dxidx[1][j];
+    }
+  } 
+}
+
+template<typename Array>
+__device__ inline static void fe_2d_assemble_term(Array &rhse,int nphi, int nf, scalar *phi, scalar dphi[3][2], scalar w, scalar *s) {
+  for (int node = 0; node < nphi; node++) { 
+    for (int field = 0; field < nf; ++field) {
+      scalar r = phi[node]*s[field];
+      if(dphi) 
+        r += dphi[node][0]*s[nf+field*2+0]+dphi[node][1]*s[nf+field*2+1];
+      rhse(field, node, 0) += w*r;
+    }
+  }
+}
+
+__device__ inline static void tri_phi(const scalar pts[2], scalar *phi) {
+  scalar xi = pts[0], eta = pts[1];
+  phi[0] = 1-xi-eta;
+  phi[1] = xi;
+  phi[2] = eta;
+}
+
+__device__ inline static void tri_phi_e(scalar pts[2], scalar *phi) {
+  scalar xi = pts[0], eta = pts[1];
+  phi[0] = 1-xi-eta;
+  phi[1] = xi;
+  phi[2] = eta;
+}
+
+template<typename Array>
+__device__ inline static void fe_2d_grad_field(Array &solution, int field, int ie, const scalar dphi[3][2], scalar v[2]){
+  for (int k = 0; k < 2; ++k){
+    v[k] = 0;
+  }
+  for (int node = 0; node <3; ++node) {
+    for (int k = 0; k < 2; ++k){
+      v[k] += solution(field, node, ie)*dphi[node][k];
+    }
+  }
+}
+
+#endif
